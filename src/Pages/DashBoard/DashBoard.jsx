0be62d2cbbf4293007cc1ad093528381b08ebac7
ThanhNgo2k1 <ngotanthanh1342001@gmail.com>
2024-09-15 23:31:37 +0700

    move sidebar to sidrbar folder

--- src/Pages/DashBoard/DashBoard.jsx
+++ src/Pages/DashBoard/DashBoard.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Sidebar from "../../Component/Sidebar";
 
 const DashBoard = () => {
   return <div className="">Dashboard</div>;
